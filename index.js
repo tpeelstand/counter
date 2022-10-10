53d3905f3a5aff57d83fa8caf63fe7646f2b1386
Nathan Ellingson <n_ellingson1@yahoo.com>
2022-10-10 14:39:58 -0500

    Adding files
    Adding css, html and javascript files to project.

---
+++ index.js
@@ -0,0 +1,32 @@
+// Variables
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+// Add people to count
+function increment() {
+    count += 1
+    countEl.textContent = count
+};
+
+// subtract people from count
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+};
+
+// Reset count to 0
+// function reset() {
+//     count = 0
+//     countEl.textContent = count
+// };
+
+// Save Button
+function save() {
+   let saveCount = count + ", ";
+   saveEl.textContent += saveCount;
+  
+   // Reset count to 0 after save
+   count = 0
+   countEl.textContent = count
+}
